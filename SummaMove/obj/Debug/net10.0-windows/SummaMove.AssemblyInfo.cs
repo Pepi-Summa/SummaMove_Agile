88234b0db2d57e640e6273963bfea6ac9a6eee18
hodan <ps262423@summacollege.nl>
2026-06-01 10:13:23 +0200

    oude versie

--- SummaMove/obj/Debug/net10.0-windows/SummaMove.AssemblyInfo.cs
+++ SummaMove/obj/Debug/net10.0-windows/SummaMove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SummaMove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814697420e78d86d7590ebc7f1a1bce2812b903a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SummaMove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SummaMove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
