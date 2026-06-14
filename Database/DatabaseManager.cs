9cc9cbe610e937d31e30e441acba373e31b59278
DJSUMMA <ps226543@summacollege.nl>
2026-06-15 00:50:48 +0200

    Backend
    backend voor de homepagina

---
+++ Database/DatabaseManager.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using SummaMove.Models;
+
+namespace SummaMove.Database
+{
+    public class DatabaseManager
+    {
+        private string connStr =
+            "server=localhost;user=root;password=;database=summamovedatabase;";
+
+        public List<Challenge> GetChallenges()
+        {
+            List<Challenge> challenges = new();
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query =
+                    "SELECT * FROM challenges LIMIT 3";
+
+                MySqlCommand cmd =
+                    new MySqlCommand(query, conn);
+
+                MySqlDataReader reader =
+                    cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    challenges.Add(new Challenge()
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Difficulty = reader["difficulty"].ToString(),
+                        Name = reader["name"].ToString(),
+                        Description = reader["description"].ToString(),
+                        PointReward = Convert.ToInt32(reader["point_reward"])
+                    });
+                }
+            }
+
+            return challenges;
+        }
+
+        public void StartChallenge(int userId, int challengeId)
+        {
+            using (MySqlConnection conn =
+                new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query =
+                @"INSERT INTO user_challenges
+                (user_id, challenge_id, completed)
+                VALUES
+                (@userId, @challengeId, false)";
+
+                MySqlCommand cmd =
+                    new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@userId", userId);
+                cmd.Parameters.AddWithValue("@challengeId", challengeId);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Homepagina.xaml.cs
+++ Homepagina.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SummaMove.Database;
+using SummaMove.Models;
 
 namespace SummaMove
 {
@@ -19,9 +22,42 @@ namespace SummaMove
     /// </summary>
     public partial class Homepagina : Window
     {
+        private DatabaseManager db = new();
+        private int currentUserId = 1;
+
         public Homepagina()
         {
             InitializeComponent();
+
+            LoadChallenges();
         }
+
+
+        private void LoadChallenges()
+        {
+            var challenges = db.GetChallenges();
+
+            ChallengesList.Items.Clear();
+
+            foreach (var challenge in challenges)
+            {
+                ChallengesList.Items.Add(
+                    $"{challenge.Name} | " +
+                    $"{challenge.Difficulty} | " +
+                    $"{challenge.PointReward} punten");
+            }
+        }
+
+        private void StartChallenge_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int challengeId = Convert.ToInt32(button.Tag);
+
+            db.StartChallenge(currentUserId, challengeId);
+
+            MessageBox.Show("Challenge gestart!");
+        }
+
     }
 }
